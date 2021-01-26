30754e994039178db8ea07096ccb974b2d655cff
AndriiSorokin <68332371+AndriiSorokin@users.noreply.github.com>
2021-01-26 13:14:51 +0200

    сделал логику для стран

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import css from "./style.css";
 import debounce from 'lodash.debounce'
 import fetchCountries from './fetchCountries'
 import template from './Template/template.hbs'
+import templateList from './Template/templateList.hbs'
 
 import {success, error,notice, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
 import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
@@ -9,50 +10,57 @@ import "../node_modules/@pnotify/core/dist/BrightTheme.css";
 import "../node_modules/@pnotify/core/dist/PNotify.css";
 defaultModules.set(PNotifyMobile, {});
 
+
 const refs = {
   list : document.querySelector('.js-list'),
   input : document.querySelector('.js-countries'),
 }
-
-refs.input.addEventListener('input', debounce(() => {
-  const inputValue = refs.input.value
-  refs.list.innerHTML = ''
-
-  fetchCountries(inputValue)
-    .then((countries) => {
-      if (countries.status == 200) {
-       success({text: 'Страна найдена'})
-      } if(countries.status == 404) {
-     error({text: 'Введите корректное название'})
-      }
-       const markup = template(countries)
-      refs.list.insertAdjacentHTML('beforeend', markup)
-    })
-}), 500)
 
 // refs.input.addEventListener('input', debounce(() => {
 //   const inputValue = refs.input.value
 //   refs.list.innerHTML = ''
 
 //   fetchCountries(inputValue)
-//     .then(updateMarkup)
-
+//     .then((countries) => {
+    //   if (countries.status == 200) {
+    //    success({text: 'Страна найдена'})
+    //   } if(countries.status == 404) {
+    //  error({text: 'Введите корректное название'})
+    //   }
+//        const markup = template(countries)
+//       refs.list.insertAdjacentHTML('beforeend', markup)
+//     })
 // }), 500)
 
-// function updateMarkup(data) {
-//  const markup = template(data)
-//   refs.list.insertAdjacentHTML('beforeend', markup)
-// }
+refs.input.addEventListener('input', debounce(() => {
+  const inputValue = refs.input.value
+  refs.list.innerHTML = ''
 
-// function Pisuccess() {
-//   const mySuccess = success({
-//     text: "I'm a success message.",
-//     delay: 1500,
-// });
-// }
+  if (inputValue.length > 0) {
+  fetchCountries(inputValue)
+    .then(data => {
+      if (data.length > 10) {
+        error({ text: 'Слишком много стран' })
+        return;
+      }
+      if (data.length < 10 && data.length > 1) {
+       return  moreCountry(data)
 
-// function Pierror() {
-//   const myError = error({
-//   text: "I'm an error message."
-// });
-// }
+      }
+      if (data.length == 1) {
+        success({text: 'Страна найдена'})
+        return updateMarkup(data)
+      }
+    })
+  }
+}, 500))
+
+function updateMarkup(data) {
+ const markup = template(data)
+  refs.list.insertAdjacentHTML('beforeend', markup)
+}
+
+function moreCountry(data) {
+  const markup = templateList(data)
+  refs.list.insertAdjacentHTML('beforeend', markup)
+}
