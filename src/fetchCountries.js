ac99e37043003817f2e38085c1572daa273d7457
AndriiSorokin <68332371+AndriiSorokin@users.noreply.github.com>
2021-01-27 17:54:07 +0200

    минимально стилей

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,30 +1,6 @@
-// import { alert,error,success, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
-// import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
-// import "../node_modules/@pnotify/core/dist/BrightTheme.css";
-// import "../node_modules/@pnotify/core/dist/PNotify.css";
-// defaultModules.set(PNotifyMobile, {});
-
 function fetchCountries(searchQuery) {
   return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
     .then((response) => response.json())
 }
 
 export default fetchCountries
-
-
-    // .then(response => {
-    //   if(response.status == 200 ){
-    //       return  response.json();
-    //   } else {
-    //     notice = alert({title:'NOT Found', hide: true, delay: 1000})
-    // }
-    // if(response.status == 404) {
-    //     error({text:`error 404`})
-    // }
-    // })
-    // .then(data => {
-    //     if(data.length > 10|| !data.length) {
-    //         return error({text: 'Введите точный запрос'})
-    //     }
-    //   if (!data.length) return error({ text: `пустой запрос` })
-    //     })
--- src/index.js
+++ src/index.js
@@ -3,34 +3,16 @@ import debounce from 'lodash.debounce'
 import fetchCountries from './fetchCountries'
 import template from './Template/template.hbs'
 import templateList from './Template/templateList.hbs'
-
-import {success, error,notice, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import {success, error, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
 import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
 import "../node_modules/@pnotify/core/dist/BrightTheme.css";
 import "../node_modules/@pnotify/core/dist/PNotify.css";
 defaultModules.set(PNotifyMobile, {});
 
-
 const refs = {
   list : document.querySelector('.js-list'),
   input : document.querySelector('.js-countries'),
 }
-
-// refs.input.addEventListener('input', debounce(() => {
-//   const inputValue = refs.input.value
-//   refs.list.innerHTML = ''
-
-//   fetchCountries(inputValue)
-//     .then((countries) => {
-    //   if (countries.status == 200) {
-    //    success({text: 'Страна найдена'})
-    //   } if(countries.status == 404) {
-    //  error({text: 'Введите корректное название'})
-    //   }
-//        const markup = template(countries)
-//       refs.list.insertAdjacentHTML('beforeend', markup)
-//     })
-// }), 500)
 
 refs.input.addEventListener('input', debounce(() => {
   const inputValue = refs.input.value
@@ -39,9 +21,13 @@ refs.input.addEventListener('input', debounce(() => {
   if (inputValue.length > 0) {
   fetchCountries(inputValue)
     .then(data => {
+      console.log(data);
       if (data.length > 10) {
         error({ text: 'Слишком много стран' })
         return;
+      }
+      if (data.status === 404) {
+      return  error({text:'Не правильный ввод'})
       }
       if (data.length < 10 && data.length > 1) {
        return  moreCountry(data)
@@ -64,3 +50,4 @@ function moreCountry(data) {
   const markup = templateList(data)
   refs.list.insertAdjacentHTML('beforeend', markup)
 }
+
